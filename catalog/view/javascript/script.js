0e9f895ad25d07a592711516a43bf06d6003f2cd
Andrey Strelcov <astrelcov@yahoo.com>
2025-01-05 00:38:59 +0200

    language pre/end

---
+++ catalog/view/javascript/script.js
@@ -0,0 +1,46 @@
+// Mobile menu
+console.log("Файл script.js успешно подключён!");
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.header-menu'),
+  menuItem = document.querySelectorAll('header-menu__item'),
+  hamburger = document.querySelector('.menu-mob');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('menu-mob__active');
+    menu.classList.toggle('header-menu__active');
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('menu-mob__active');
+      menu.classList.toggle('header-menu__active');
+    })
+  })
+})
+// Mobile menu end
+
+
+
+// Получаем элементы
+const openSearch = document.getElementById('openSearch');
+const closeSearch = document.getElementById('closeSearch');
+const searchPopup = document.getElementById('searchPopup');
+
+// Открытие поиска
+openSearch.addEventListener('click', () => {
+  searchPopup.style.display = 'flex';
+});
+
+// Закрытие поиска
+closeSearch.addEventListener('click', () => {
+  searchPopup.style.display = 'none';
+});
+
+// Закрытие по клику вне области окна
+window.addEventListener('click', (event) => {
+  if (event.target === searchPopup) {
+    searchPopup.style.display = 'none';
+  }
+});
+
