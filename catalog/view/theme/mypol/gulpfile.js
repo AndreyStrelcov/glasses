fcbd9775833744224f02fa60cf58605bace996d6
Andrey Strelcov <astrelcov@yahoo.com>
2025-01-07 23:07:13 +0200

    last edited gulp

--- catalog/view/theme/mypol/gulpfile.js
+++ catalog/view/theme/mypol/gulpfile.js
@@ -1,28 +1,63 @@
+// const gulp = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+
+// // Пути к файлам
+// const paths = {
+//     styles: {
+//         src: 'catalog/view/theme/mypol/scss/**/*.scss', // SCSS файлы
+//         dest: 'catalog/view/theme/mypol/stylesheet/',   // Папка для CSS
+//     },
+// };
+
+// // Компиляция SCSS в CSS
+// function styles() {
+//     return gulp
+//         .src(paths.styles.src)
+//         .pipe(sass().on('error', sass.logError)) // Лог ошибок Sass
+//         .pipe(gulp.dest(paths.styles.dest)) // Вывод CSS
+//         .on('end', () => console.log('SCSS компиляция завершена!')); // Лог успешной компиляции
+// }
+
+// // Наблюдение за изменениями
+// function watch() {
+//     gulp.watch(paths.styles.src, styles);
+// }
+
+// // Задачи по умолчанию
+// exports.styles = styles;
+// exports.watch = watch;
+// exports.default = watch;
+
+
+
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
-// Пути к файлам
 const paths = {
-    styles: {
-        src: 'catalog/view/theme/mypol/scss/**/*.scss', // SCSS файлы
-        dest: 'catalog/view/theme/mypol/stylesheet/',   // Папка для CSS
-    },
+    scss: './scss/**/*.scss', // Все SCSS-файлы
+    css: './stylesheet/',     // Папка назначения для CSS
 };
 
-// Компиляция SCSS в CSS
+// Задача для компиляции SCSS
 function styles() {
-    return gulp
-        .src(paths.styles.src) // Источник
-        .pipe(sass().on('error', sass.logError)) // Компиляция SCSS
-        .pipe(gulp.dest(paths.styles.dest)); // Вывод CSS
+    return gulp.src('./scss/main.scss') // Основной SCSS-файл
+        .pipe(sourcemaps.init())        // Инициализация sourcemaps
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError)) // Компиляция SCSS
+        .pipe(sourcemaps.write('./'))  // Запись sourcemaps
+        .pipe(gulp.dest(paths.css));   // Сохранение в папку назначения
 }
 
 // Наблюдение за изменениями
 function watch() {
-    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.scss, styles);
 }
 
 // Задачи по умолчанию
 exports.styles = styles;
 exports.watch = watch;
-exports.default = watch;
+exports.default = gulp.series(styles, watch);
+
+
+
